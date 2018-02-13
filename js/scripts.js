4eee1965c26bc5a8447ed142fffb8642f1a0c74f
Victor Felix <vfelix82@gmail.com>
2018-02-13 08:03:33 -0800

    html javascript practice.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var leapYear = function(year) {
+  if ((year % 4 == 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    if (result == true) {
+      $("#result").text(year + " is a leap year.");
+    } else {
+      $("#result").text(year + " is not a leap year.");
+    }
+  });
+});
